c8a333221bab23cda54b1d4dbc6e69914fb600cb
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-06-22 00:15:51 -0400

    Added weapon group anims

--- client.lua
+++ client.lua
@@ -1,12 +1,58 @@
-local animDict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c"
-local animName = "fidget_med_loop"
+local weaponAnimations = {
+    [`GROUP_PISTOL`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_THROWN`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_SHOTGUN`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_SMG`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_RIFLE`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_SNIPER`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_MELEE`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+    },
+    [`GROUP_MG`] = {
+    dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+    anim = "fidget_med_loop"
+    },
+    [`GROUP_STUNGUN`] = {
+        dict = "weapons@first_person@aim_idle@p_m_zero@pistol@shared@fidgets@c",
+        anim = "fidget_med_loop"
+        
+    },
+}
+
 
 local function playFidgetAnim()
     local ped = PlayerPedId()
 
-    if not IsPedArmed(ped, 4) or IsEntityPlayingAnim(ped, animDict, animName, 3) then
-        return
-    end
+    local weapon = GetSelectedPedWeapon(ped)
+    local weaponGroup = GetWeapontypeGroup(weapon)
+    local animData = weaponAnimations[weaponGroup]
+
+    if not animData then return end
+
+    local animDict = animData.dict
+    local animName = animData.anim
+
+    if not IsPedArmed(ped, 7) then return end
+    if IsEntityPlayingAnim(ped, animDict, animName, 3) then return end
 
     if not HasAnimDictLoaded(animDict) then
         RequestAnimDict(animDict)
@@ -15,13 +61,12 @@ local function playFidgetAnim()
         end
     end
 
-
     TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, 48, 0, false, false, false)
 
-        CreateThread(function()
+    CreateThread(function()
         while IsEntityPlayingAnim(ped, animDict, animName, 3) do
             if IsPedShooting(ped) then
-                ClearPedTasks(ped)
+                StopAnimTask(ped, animDict, animName, 1.0)
                 break
             end
             Wait(0)
@@ -29,9 +74,8 @@ local function playFidgetAnim()
     end)
 end
 
-
 RegisterCommand("_fidgetanim", function()
     playFidgetAnim()
 end, false)
 
-RegisterKeyMapping("_fidgetanim", "Play Fidget Animation", "MOUSE_BUTTON", "MOUSE_MIDDLE")
+RegisterKeyMapping("_fidgetanim", "Play Weapon Inspect Animation", "MOUSE_BUTTON", "MOUSE_MIDDLE")
